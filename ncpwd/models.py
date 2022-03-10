2f665dc9bbcc81af17783e32c8ea0508978de20c
FITSTARGILLY2643 <gmutai1194@gmail.com>
2022-03-10 12:34:13 +0300

    Add user model

--- ncpwd/models.py
+++ ncpwd/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=200)
+    national_id = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    phone_no = models.CharField(max_length=200)
+    reg_no = models.CharField(max_length=20)
+    county = models.CharField(max_length=20)
+    sub_county = models.CharField(max_length=20)
+    village = models.CharField(max_length=20)
+
+    class Meta:
+        ordering = ["-pk"]
+
+
+
